cfbab88d7fea5aa2e484d8b5d937ed1ca49c7415
TefaSalcedo <stefa.dtbu@gmail.com>
2024-03-06 19:09:11 -0500

    Footer component complete

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from "./components/navBar.jsx"
 import Home from './components/home.jsx'
+
+import Footer from "./components/footer.jsx"
 import "./App.css"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
@@ -9,6 +11,8 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <>
     <App/>
     <Home/>
+
+    <Footer/>
     </>
   </React.StrictMode>,
 )
